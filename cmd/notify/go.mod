e21bf59daac16a2158c5fadbd142895e080889a4
CuiYao <631836754@qq.com>
2024-03-06 14:53:03 +0800

    Update terminal-notifier to 2.0.0 and add execute option

---
+++ cmd/notify/go.mod
@@ -0,0 +1,5 @@
+module notify
+
+go 1.21.5
+
+require github.com/CuiYao631/gosx-notifier v0.0.0-20240306063026-af1fd76a95ec
--- cmd/notify/main.go
+++ cmd/notify/main.go
@@ -1,24 +1,42 @@
 package main
 
 import (
+	gosxnotifier "github.com/CuiYao631/gosx-notifier"
 	"log"
-	"os"
-	"strings"
-
-	"github.com/deckarep/gosx-notifier"
 )
 
 func main() {
-	notification := strings.Join(os.Args[1:], " ")
+	note := gosxnotifier.NewNotification("Check your Apple Stock!")
 
-	note := gosxnotifier.NewNotification(notification)
+	//(可选) 设置标题
+	note.Title = "title"
 
-	note.Title = "Notify"
+	//(可选) 设置字幕
+	note.Subtitle = "message"
 
+	//(可选) 从预定义的集合中设置声音.
+	note.Sound = gosxnotifier.Default
+
+	//(可选) 设置一个组，确保只显示一个通知，替换同一组id的先前通知.
+	note.Group = "com.wails.DevMate"
+
+	//(可选) 设置发件人 (通知现在将使用Safari图标)
+	note.Sender = "com.wails.DevMate"
+
+	//(可选) 指定要在单击通知时打开的url或bundleid。
+	note.Link = "http://finance.yahoo.com/q?s=AAPL" //or BundleID like: com.apple.Terminal
+
+	//(可选) 应用程序图标 (仅限10.9)
+	//note.AppIcon = "build/windows/icon.ico"
+
+	//(可选) 内容图像 (仅限10.9)
+	note.ContentImage = "build/appicon.png"
+
+	//然后，推送通知
 	err := note.Push()
 
-	//If necessary, check error
+	//检查错误
 	if err != nil {
-		log.Println("Uh oh! Error with Notify")
+		log.Println("Uh oh!")
 	}
 }
